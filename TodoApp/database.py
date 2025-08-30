243d05269bdf41b0e3e216448352b8ef832c7416
chankyswami <c.innovator@gmail.com>
2025-08-31 01:18:21 +0530

    database string for azure

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -2,7 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:pagal@localhost/TodoApplicationDatabase'
+# SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:pagal@localhost/TodoApplicationDatabase'
+SQLALCHEMY_DATABASE_URL = "postgresql://chanky:Number12345@apppostgresql.postgres.database.azure.com:5432/TodoApplicationDatabase?sslmode=require"
+
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
